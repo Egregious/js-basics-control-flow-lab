6510fa38e9ae2253c21cebf3fbb1411199734d3f
Egregious <dvdscheibel@yahoo.com>
2021-04-06 15:38:49 -0400

    commit tree e271dcd5eb73078cb14553325caaadaa515784e0

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(feet){
   // Write your code here!
+  if (feet <= 400) 
+    return "This one is on me!"
+  else if (feet > 2000 && feet < 2500)
+    return "I will gladly take your thirty bucks."
+  else (feet > 2500)
+    return "No can do."
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  let isNYC;
+  return city == "NYC" ? (isNYC = "Ok, sounds good.") : (isNYC = "No go.");
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  let response;
+
+  switch (tip) {
+    case 'generous':
+      return "Thank you so much.";
+      break;
+    case 'not as generous':
+      return "Thank you.";
+      break;
+    default :
+      return "Bye.";
+      break;
+  }
 }
